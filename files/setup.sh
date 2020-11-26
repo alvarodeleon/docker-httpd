bd347b7509256e0218b6a4ef163482e3123cee26
Alvaro <9125409+alvarodeleon@users.noreply.github.com>
2020-11-26 19:26:42 -0300

    Include PHP 8.0 y Error Fixs

--- files/setup.sh
+++ files/setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# File              : setup.sh
+# Author            : Alvaro <alvaro@server.com.uy>
+# Date              : 26.11.2020
+# Last Modified Date: 26.11.2020
+# Last Modified By  : Alvaro <alvaro@server.com.uy>
 yum -y --setopt=tsflags=nodocs update && \
     yum clean all
 
@@ -24,8 +30,8 @@ yum install -y postgresql-server postgresql postgresql-contrib
 #Con esto instalaremos PHP 5.4, 5.5, 5.6, 7.0, 7.1, 7.2 y 7.3 
 #ademas de todas las librerías necesarias mas PHP-FPM para
 #cada versión de PHP anterior
-yum install -y php54 php55 php56 php70 php71 php72 php73 \
-php54-php-fpm php55-php-fpm php56-php-fpm php70-php-fpm php71-php-fpm php72-php-fpm php73-php-fpm \
+yum install -y php54 php55 php56 php70 php71 php72 php73 php80 \
+php54-php-fpm php55-php-fpm php56-php-fpm php70-php-fpm php71-php-fpm php72-php-fpm php73-php-fpm php80-php-fpm \
 php54-php-mysql php54-php-mcrypt php54-php-mbstring php54-php-cli php54-php-fpm php54-php-gd php54-php-json php54-php-ioncube-loader php54-php-intl php54-php-pdo php54-php-pgsql php54-php-soap php54-php-xml php54-php-xmlrpc \
 php55-php-mysql php55-php-mcrypt php55-php-mbstring php55-php-cli php55-php-fpm php55-php-gd php55-php-json php55-php-ioncube-loader php55-php-intl php55-php-pdo php55-php-pgsql php55-php-soap php55-php-xml php55-php-xmlrpc \
 php56-php-mysql php56-php-mcrypt php56-php-mbstring php56-php-cli php56-php-fpm php56-php-gd php56-php-json php56-php-ioncube-loader php56-php-intl php56-php-pdo php56-php-pgsql php56-php-soap php56-php-xml php56-php-xmlrpc \
@@ -35,9 +41,10 @@ php72-php-mysql php72-php-mcrypt php72-php-mbstring php72-php-cli php72-php-fpm 
 php73-php-mysql php73-php-mcrypt php73-php-mbstring php73-php-cli php73-php-fpm php73-php-gd php73-php-json php73-php-ioncube-loader php73-php-intl php73-php-pdo php73-php-pgsql php73-php-soap php73-php-xml php73-php-xmlrpc \
 php73-php-mysql php73-php-mcrypt php73-php-mbstring php73-php-cli php73-php-fpm php73-php-gd php73-php-json php73-php-ioncube-loader php73-php-intl php73-php-pdo php73-php-pgsql php73-php-soap php73-php-xml php73-php-xmlrpc \
 php74-php-mysql php74-php-mcrypt php74-php-mbstring php74-php-cli php74-php-fpm php74-php-gd php74-php-json php74-php-ioncube-loader php74-php-intl php74-php-pdo php74-php-pgsql php74-php-soap php74-php-xml php74-php-xmlrpc \
+php80-php-mysqlnd php80-php-common php80-php-mbstring php80-php-cli php80-php-fpm php80-php-gd php80-php-json php80-php-ioncube-loader php80-php-intl php80-php-pdo php80-php-pgsql php80-php-soap php80-php-xml php80-php-pecl-xmlrpc
 
 #Agregador sporte SSH2
-yum install -y libssh2-devel php54-php-pecl-ssh2 php55-php-pecl-ssh2 php56-php-pecl-ssh2 php70-php-pecl-ssh2 php71-php-pecl-ssh2 php72-php-pecl-ssh2 php73-php-pecl-ssh2 php74-php-pecl-ssh2
+yum install -y libssh2-devel php54-php-pecl-ssh2 php55-php-pecl-ssh2 php56-php-pecl-ssh2 php70-php-pecl-ssh2 php71-php-pecl-ssh2 php72-php-pecl-ssh2 php73-php-pecl-ssh2 php74-php-pecl-ssh2 php80-php-pecl-ssh2
 
 
 yum -y install python2-pip python34 python34-pip python36 python36-pip
@@ -54,6 +61,7 @@ echo "extension=ssh2.so" >> /etc/opt/remi/php71/php.ini
 echo "extension=ssh2.so" >> /etc/opt/remi/php72/php.ini
 echo "extension=ssh2.so" >> /etc/opt/remi/php73/php.ini
 echo "extension=ssh2.so" >> /etc/opt/remi/php74/php.ini
+echo "extension=ssh2.so" >> /etc/opt/remi/php80/php.ini
 
 adduser www-data
 
@@ -81,6 +89,7 @@ sed -i 's/:9000/:9071/' /etc/opt/remi/php71/php-fpm.d/www.conf
 sed -i 's/:9000/:9072/' /etc/opt/remi/php72/php-fpm.d/www.conf
 sed -i 's/:9000/:9073/' /etc/opt/remi/php73/php-fpm.d/www.conf
 sed -i 's/:9000/:9074/' /etc/opt/remi/php74/php-fpm.d/www.conf
+sed -i 's/:9000/:9080/' /etc/opt/remi/php80/php-fpm.d/www.conf
  
 #Cambiando usuario de PHP-FPM
 sed -i 's/user = apache/user = www-data/' /opt/remi/php54/root/etc/php-fpm.d/www.conf
@@ -97,6 +106,10 @@ sed -i 's/user = apache/user = www-data/' /etc/opt/remi/php72/php-fpm.d/www.conf
 sed -i 's/group = apache/group = www-data/' /etc/opt/remi/php72/php-fpm.d/www.conf
 sed -i 's/user = apache/user = www-data/' /etc/opt/remi/php73/php-fpm.d/www.conf
 sed -i 's/group = apache/group = www-data/' /etc/opt/remi/php73/php-fpm.d/www.conf
+sed -i 's/user = apache/user = www-data/' /etc/opt/remi/php74/php-fpm.d/www.conf
+sed -i 's/group = apache/group = www-data/' /etc/opt/remi/php74/php-fpm.d/www.conf
+sed -i 's/user = apache/user = www-data/' /etc/opt/remi/php80/php-fpm.d/www.conf
+sed -i 's/group = apache/group = www-data/' /etc/opt/remi/php80/php-fpm.d/www.conf
 
 #Cambiamos propietraios y grupos
 chown www-data /opt/remi/php54/root/var/log/php-fpm
@@ -106,6 +119,8 @@ chown www-data /var/opt/remi/php70/log/php-fpm
 chown www-data /var/opt/remi/php71/log/php-fpm
 chown www-data /var/opt/remi/php72/log/php-fpm
 chown www-data /var/opt/remi/php73/log/php-fpm
+chown www-data /var/opt/remi/php74/log/php-fpm
+chown www-data /var/opt/remi/php80/log/php-fpm
 
 chgrp www-data -R /opt/remi/php54/root/var/lib/php/wsdlcache
 chgrp www-data -R /opt/remi/php56/root/var/lib/php/wsdlcache
@@ -118,6 +133,10 @@ chgrp www-data -R /var/opt/remi/php71/lib/php/wsdlcache
 chgrp www-data -R /var/opt/remi/php71/lib/php/opcache
 chgrp www-data -R /var/opt/remi/php73/lib/php/wsdlcache
 chgrp www-data -R /var/opt/remi/php73/lib/php/opcache
+chgrp www-data -R /var/opt/remi/php74/lib/php/wsdlcache
+chgrp www-data -R /var/opt/remi/php74/lib/php/opcache
+chgrp www-data -R /var/opt/remi/php80/lib/php/wsdlcache
+chgrp www-data -R /var/opt/remi/php80/lib/php/opcache
 
 chown www-data:www-data /opt/remi/php54/root/var/lib/php/session
 chown www-data:www-data /opt/remi/php55/root/var/lib/php/session
@@ -125,7 +144,8 @@ chown www-data:www-data /var/opt/remi/php56/lib/php/session
 chown www-data:www-data /var/opt/remi/php70/lib/php/session
 chown www-data:www-data /var/opt/remi/php71/lib/php/session
 chown www-data:www-data /var/opt/remi/php72/lib/php/session
-chown www-data:nginx /var/opt/remi/php73/lib/php/session
+chown www-data:www-data /var/opt/remi/php73/lib/php/session
+chown www-data:www-data /var/opt/remi/php80/lib/php/session
 
 sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php54-php-fpm.service
 sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php55-php-fpm.service
@@ -134,6 +154,7 @@ sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php70-php-
 sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php71-php-fpm.service
 sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php72-php-fpm.service
 sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php73-php-fpm.service
+sed -i 's/PrivateTmp=true/PrivateTmp=false/g' /usr/lib/systemd/system/php80-php-fpm.service
 
 echo -en "\033[1;31m" >> /etc/issue
 echo "Default" >> /etc/issue
